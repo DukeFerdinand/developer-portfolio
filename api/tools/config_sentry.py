906d4ad777e215adc2d5da2a5f0707083c92abfe
Douglass Hooker <DouglassHooker@gmail.com>
2019-06-24 09:37:30 -0400

    Remove debug from sentry config

--- api/tools/config_sentry.py
+++ api/tools/config_sentry.py
@@ -3,7 +3,6 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def init_sentry(sentry_config):
-    print(sentry_config)
     sentry_sdk.init(
         dsn=sentry_config["SENTRY_DSN"],
         release=sentry_config["RELEASE"],
